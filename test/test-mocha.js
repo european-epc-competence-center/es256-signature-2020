ca49b589c94d194c65389498b3e83af75ebab9f1
Yannik Sinthern <yannik.sinthern@eecc.de>
2025-11-10 10:26:42 +0100

    fix pipeline test

---
+++ test/test-mocha.js
@@ -0,0 +1,12 @@
+/*!
+ * Copyright (c) 2024 Christian Fries. All rights reserved.
+ */
+
+// Test setup file for Mocha
+// This file is loaded before any tests run
+
+// Register ts-node to handle TypeScript files
+require('ts-node/register');
+
+// Set up any global test configuration here if needed
+

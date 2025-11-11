b43c2ecf103b04e44f05a4c0875ae0053138fd42
Yannik Sinthern <yannik.sinthern@eecc.de>
2025-11-11 14:51:58 +0100

    fix pipeline test

--- test/test-mocha.js
+++ test/test-mocha.js
@@ -5,8 +5,15 @@
 // Test setup file for Mocha
 // This file is loaded before any tests run
 
-// Register ts-node to handle TypeScript files
-require('ts-node/register');
+// Register ts-node to handle TypeScript files with ES module interop
+require('ts-node').register({
+  transpileOnly: true,
+  compilerOptions: {
+    module: 'commonjs',
+    esModuleInterop: true,
+    allowSyntheticDefaultImports: true
+  }
+});
 
 // Set up any global test configuration here if needed
 
